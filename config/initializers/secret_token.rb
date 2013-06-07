cc379a398f25427bc2bdc5538d8ed1a6ec19ac83
claire allison <callison22@gmail.com>
2013-06-07 09:20:22 -0400

    Initial commit for CIBT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CIBT::Application.config.secret_token = 'c08ba1374e8f0c8a633f2ecd3e2290ed8db8164cb15269ef33c781873cbdbd84ab8ad36672c129a54e944c3fa9566836f682908903d5ca7245f16a28797a8e26'
